a57cae0187f5015cdaa6d893760c76b75ecb70f2
drakulacyber <79672405+drakulacyber@users.noreply.github.com>
2026-09-11 15:18:20 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Menggunakan relative path agar bekerja di localhost dan GitHub Pages subpath
   server: {
     port: 3000,
     host: true
